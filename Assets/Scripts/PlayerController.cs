8fc62b6f99dcc5468da2d36a124a2938dfdba7bc
Pyush Chamoli <pyushthebeast@gmail.com>
2023-04-28 16:52:23 +0200

    Completely revamped movement to use acceleration

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,25 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+
 public class PlayerController : MonoBehaviour
 {
-    public CharacterController characterController;
-    public float moveSpeed = 5f;
-    public float jumpVelocity = 10f;
-    // Start is called before the first frame update
+    private CharacterController _characterController;
+    
+    [SerializeField] private float _maxVelocity = 1f;
+    [SerializeField] private float _moveAcceleration = 1f;
+    [SerializeField] private float _moveDeceleration = 5f;
+
+    [SerializeField] private float _groundedDownwardVelocity = 0.01f;
+
+    [SerializeField] private Vector3 _playerVelocity;
+
+    [SerializeField] private float _gravitationalAcceleration;
+    [SerializeField] private float _jumpVelocity = 10f;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
     void Start()
     {
-        
+        _gravitationalAcceleration = Physics.gravity.magnitude;
     }
 
     // Update is called once per frame
     void Update()
     {
-        characterController.Move(new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical")) * moveSpeed * Time.deltaTime);
-        characterController.Move(Physics.gravity * Time.deltaTime);
-        if (Input.GetKeyDown(KeyCode.Space))
+        _playerVelocity += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * _moveAcceleration * Time.deltaTime;
+        if (_playerVelocity.sqrMagnitude > _maxVelocity*_maxVelocity)
         {
-            characterController.Move(Vector3.up * jumpVelocity);
+            _playerVelocity = _playerVelocity.normalized * _maxVelocity;
         }
+
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            Vector3 velocityChange = new Vector3(_playerVelocity.x,0,_playerVelocity.z) * _moveDeceleration * Time.deltaTime;
+            if (_playerVelocity.sqrMagnitude - velocityChange.sqrMagnitude > 0) //If deceleration won't make it go in opposite direction
+            {
+                _playerVelocity -= velocityChange;
+            }
+            else
+            {
+                _playerVelocity = new Vector3(0, _playerVelocity.y, 0);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Space) && _characterController.isGrounded && _playerVelocity.y <= 0)
+        {
+            _playerVelocity += Vector3.up * _jumpVelocity;
+        }
+        else if (_characterController.isGrounded == false)
+        {
+            _playerVelocity += Vector3.down * Time.deltaTime * _gravitationalAcceleration;
+        }
+        else
+        {
+            _playerVelocity = new Vector3(_playerVelocity.x, -_groundedDownwardVelocity, _playerVelocity.z);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        _characterController.Move(_playerVelocity*Time.deltaTime);
     }
 }

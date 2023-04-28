df6e1868ff9f4b4d48ed7b7eb6db44fdca69dcdd
Pyush Chamoli <pyushthebeast@gmail.com>
2023-04-28 23:54:59 +0200

    Added camera collisions

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+    private Camera _mainCamera;
+    private float _offsetDistance;
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _mouseSenstivity = 100f;
+    [SerializeField] private float _slerpFactor = 0.01f;
+    [SerializeField] private Quaternion _targetRotation;
+    [SerializeField] private Transform _cameraHolder;
+
+    [SerializeField] private float _angleX;
+    [SerializeField] private float _angleY;
+    [SerializeField] private float _deltaAngleX = 0;
+    [SerializeField] private float _deltaAngleY = 0;
+
+    [SerializeField] private float _maximumClipping = 10f;
+    [SerializeField] private float _clipOffset = 1f;
+    [SerializeField] private Vector3 _cameraCentreViewPort = new Vector3(0.5f, 0.5f, 0.05f);
+
+    [SerializeField] private Vector2 _yLimits = new Vector2(-90, 90);
+
+    [SerializeField] private float _clipLerp = 10f;
+    [SerializeField] private Vector3 _mainCameraStartingLocalPosition;
+
+    [SerializeField] private LayerMask _cameraCollision;
+    [SerializeField] private Transform _cameraClipResetPosition;
+
+    private Vector2 _mouseInput;
+    private void Awake()
+    {
+        _mainCamera = GetComponent<Camera>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _offsetDistance = (_target.position - this.transform.position).magnitude;
+        _targetRotation = this.transform.rotation;
+        _mainCameraStartingLocalPosition = _mainCamera.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        _deltaAngleX = _mouseInput.x * Time.deltaTime * _mouseSenstivity;
+        _angleX += _deltaAngleX;
+        
+        float _testDeltaY = _mouseInput.y * Time.deltaTime * _mouseSenstivity;
+        if (_testDeltaY + _angleY < _yLimits.y && _testDeltaY + _angleY > _yLimits.x)
+        {
+            _deltaAngleY = _testDeltaY;
+        }
+        else if (_testDeltaY < 0)
+        {
+            _testDeltaY = _angleY - _yLimits.x;
+        }
+        else
+        {
+            _testDeltaY = _yLimits.y - _angleY;
+        }
+        _deltaAngleY = _testDeltaY;
+        _angleY += _deltaAngleY;
+
+        ClipCamera();
+        //Mathf.Clamp(_angleY, _yLimits.x, _yLimits.y);
+        //_targetRotation = Quaternion.Euler(_angleY, _angleX, 0);
+}
+
+
+    private void ClipCamera()
+    {
+        Vector3 cameraPosition = _mainCamera.ViewportToWorldPoint(_cameraCentreViewPort);
+        if (Physics.Linecast(cameraPosition, _target.root.position, out RaycastHit hit,_cameraCollision))
+        {
+            float MoveDistance = 0f;
+            if (Physics.Linecast(_target.root.position, cameraPosition, out RaycastHit hitBack, _cameraCollision))
+            {
+                MoveDistance = (_cameraHolder.transform.position - hitBack.transform.position).magnitude + _clipOffset;
+                /*_cameraHolder.transform.localPosition = Vector3.Lerp(transform.localPosition, transform.localPosition
+                    + (_target.root.position - transform.position).normalized * MoveDistance, _clipLerp * Time.deltaTime);*/
+                _mainCamera.transform.localPosition += new Vector3(0, 0, _clipLerp * Time.deltaTime * MoveDistance);
+                print("Clipped");
+            }
+            else 
+            {
+                if (Physics.Linecast(_target.root.position, _cameraClipResetPosition.position, _cameraCollision))
+                {
+                    return;
+                }
+                ClipBack();
+            }
+
+        }
+        else
+        {
+            if (Physics.Linecast(_target.root.position, _cameraClipResetPosition.position, _cameraCollision))
+            {
+                return;
+            }
+            ClipBack();
+        }
+    }
+
+    private void ClipBack()
+    {
+        _mainCamera.transform.localPosition = Vector3.Lerp(this.transform.localPosition, _mainCameraStartingLocalPosition, _clipLerp * Time.deltaTime);
+    }
+
+    private void LateUpdate() //So that camera moves after player
+    {
+        transform.root.Rotate(0, _deltaAngleX, 0);
+        _cameraHolder.transform.Rotate(-_deltaAngleY, 0, 0);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,6 +19,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _gravitationalAcceleration;
     [SerializeField] private float _jumpVelocity = 10f;
 
+    private Vector2 _Input = Vector2.zero;
+
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
@@ -31,30 +33,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _playerVelocity += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * _moveAcceleration * Time.deltaTime;
-        if (_playerVelocity.sqrMagnitude > _maxVelocity*_maxVelocity)
-        {
-            _playerVelocity = _playerVelocity.normalized * _maxVelocity;
-        }
+        SetInput();
 
-        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
-        {
-            Vector3 velocityChange = new Vector3(_playerVelocity.x,0,_playerVelocity.z) * _moveDeceleration * Time.deltaTime;
-            if (_playerVelocity.sqrMagnitude - velocityChange.sqrMagnitude > 0) //If deceleration won't make it go in opposite direction
-            {
-                _playerVelocity -= velocityChange;
-            }
-            else
-            {
-                _playerVelocity = new Vector3(0, _playerVelocity.y, 0);
-            }
-        }
+        SetPlanarVelocity(); //As in, not jumping on the Y-Axis
 
-        if (Input.GetKey(KeyCode.Space) && _characterController.isGrounded && _playerVelocity.y <= 0)
+        if (CanJump())
         {
             _playerVelocity += Vector3.up * _jumpVelocity;
         }
-        else if (_characterController.isGrounded == false)
+        else if (IsGrounded() == false)
         {
             _playerVelocity += Vector3.down * Time.deltaTime * _gravitationalAcceleration;
         }
@@ -65,8 +52,47 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    private bool IsGrounded()
+    {
+        return _characterController.isGrounded;
+    }
+
+    private bool CanJump()
+    {
+        return Input.GetKey(KeyCode.Space) && _characterController.isGrounded && _playerVelocity.y <= 0;
+    }
+
+    private void SetPlanarVelocity()
+    {
+        _playerVelocity += new Vector3(_Input.x, 0, _Input.y) * _moveAcceleration * Time.deltaTime;
+        if (_playerVelocity.sqrMagnitude > _maxVelocity * _maxVelocity)
+        {
+            _playerVelocity = _playerVelocity.normalized * _maxVelocity;
+        }
+
+        if (_Input.x == 0 && _Input.y == 0)
+        {
+            Vector3 velocityChange = new Vector3(_playerVelocity.x, 0, _playerVelocity.z) * _moveDeceleration * Time.deltaTime;
+            if (_playerVelocity.sqrMagnitude - velocityChange.sqrMagnitude > 0) //If deceleration won't make it go in opposite direction
+            {
+                _playerVelocity -= velocityChange;
+            }
+            else
+            {
+                _playerVelocity = new Vector3(0, _playerVelocity.y, 0);
+            }
+        }
+    }
+
+    private void SetInput()
+    {
+        _Input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
     private void FixedUpdate()
     {
-        _characterController.Move(_playerVelocity*Time.deltaTime);
+        Vector3 _movement = Vector3.zero;
+        _movement += transform.right * _playerVelocity.x + transform.up * _playerVelocity.y + transform.forward * _playerVelocity.z;
+        _characterController.Move(_movement * Time.deltaTime);
     }
 }

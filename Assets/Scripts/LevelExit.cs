d2088608e87006194c84b50f786e0bb0233e35b4
Pyush Chamoli <pyushthebeast@gmail.com>
2023-05-13 15:20:43 +0200

    Add finish state. update models

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameManager.gameWon = true;
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
